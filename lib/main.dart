5581604529e9155087a1c5471a7870eb0aebd9ed
Eutalia <eutaliapistrui@gmail.com>
2024-11-18 14:19:58 +0200

    Lab4

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:doctorappointment_v3/presentation/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
